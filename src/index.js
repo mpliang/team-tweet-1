c1c34dbf7dee57c17cab278650fdbf9778348e5a
jbenkual <joe@benkual.com>
2015-09-16 16:51:50 -0700

    Close #3 Make list of accounts dynamic

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import authorizeGuests from "./authorize";
 
 let app = angular.module("team-tweet", []);
 
-app.controller("TweetCtrl", () => {
+app.controller("TweetCtrl", ($scope, accountService) => {
+	accountService.add("@Acct1");
+	accountService.add("@Acct2");
+	accountService.add("@Acct3");
+
+	$scope.accountList = accountService.getAll();
+});
+
+app.service("accountService", function() {
+	var accounts = [];
+	this.getAll = function () {
+		return accounts;
+	};
+
+	this.add = function(username) {
+		accounts.push(username);
+	};
 });
 
 authorizeGuests();
