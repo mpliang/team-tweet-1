9b99dbe09e0d698bee9077b38748bb5aced43ae2
Samer Buna <samer.buna@gmail.com>
2015-09-16 15:32:52 -0700

    Close #1
    Allow users to authorize with Twitter, and remember their account with localStorage

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 let hot = "HOT";
 console.log(`ES2015 is ${hot}`);
+
+let authorizeGuests = () => {
+  if ( localStorage["firebase:session::teamtweet15"] ) {
+    return;
+  }
+
+  var ref = new Firebase("https://teamtweet15.firebaseio.com");
+  ref.authWithOAuthPopup("twitter", function(error, authData) {
+    if (error) {
+      alert("Login Failed!", error);
+    } else {
+      alert("Authenticated successfully with payload:", authData);
+    }
+  });
+};
+
+
+authorizeGuests();
