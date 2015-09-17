20d5eccdece6ea31d0ce0f732d00d241db623323
Samer Buna <samer.buna@gmail.com>
2015-09-17 14:17:48 -0700

    Move the home into a route

--- src/ngApp.js
+++ src/ngApp.js
@@ -1,1 +1,14 @@
-window.app = angular.module("team-tweet", []);
+window.app = angular.module("team-tweet", ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'templates/home.html',
+      controller: "TweetCtrl"
+    })
+
+});
