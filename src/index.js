6623a603e778b765954fb50cdf7ff643b8a8f35a
Samer Buna <samer.buna@gmail.com>
2015-09-16 17:11:42 -0700

    Format code #3

--- src/index.js
+++ src/index.js
@@ -3,25 +3,25 @@ import authorizeGuests from "./authorize";
 let app = angular.module("team-tweet", []);
 
 app.controller("TweetCtrl", ($scope, accountService) => {
-	accountService.add("@Acct1");
-	accountService.add("@Acct2");
-	accountService.add("@Acct3");
-	accountService.add("@Acct3"); // Duplicates are ignored
+  accountService.add("@Acct1");
+  accountService.add("@Acct2");
+  accountService.add("@Acct3");
+  accountService.add("@Acct3"); // Duplicates are ignored
 
-	$scope.accountList = accountService.getAll();
+  $scope.accountList = accountService.getAll();
 });
 
 app.service("accountService", function() {
-	var accounts = [];
-	this.getAll = function () {
-		return accounts;
-	};
+  var accounts = [];
+  this.getAll = function () {
+    return accounts;
+  };
 
-	this.add = function(username) {
-		if(accounts.indexOf(username) === -1){
-			accounts.push(username);
-		}
-	};
+  this.add = function(username) {
+    if(accounts.indexOf(username) === -1){
+      accounts.push(username);
+    }
+  };
 });
 
 authorizeGuests();
